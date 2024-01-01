869c4af1e3ed1c4f6c248e4646bb37a3cca5ea1e
shanewu2002 <100037468+shanewu2002@users.noreply.github.com>
2024-01-01 22:00:39 +0800

    Add files via upload

--- regfile.v
+++ regfile.v
@@ -3,39 +3,37 @@ module regfile(
 	input srst_n,
 	input [4:0] raddr1,
 	input [4:0] raddr2,
-	input write,
+	input wen,
 	input [4:0]  waddr,
 	input [31:0] wdata,
-	output reg [31:0] rdata1,
-	output reg [31:0] rdata2,
-	output [31:0] sw_data
+	output [31:0] rdata1,
+	output [31:0] rdata2
+	// output [31:0] sw_data
 );
 
-reg [31:0] reg   [31:0];
-reg [31:0] reg_n [31:0];
+reg [31:0] gpr   [31:0];
+reg [31:0] gpr_n [31:0];
 integer i;
-assign sw_data = reg[raddr2];
+assign rdata1 = gpr[raddr1];
+assign rdata2 = gpr[raddr2];
+// assign sw_data = gpr[raddr2];
 
 always @(posedge clk) begin
     if (~srst_n) begin
         for(i = 0; i < 32; i = i + 1) begin
-		    reg[i] <= 0;
+		    gpr[i] <= 0;
 	    end
-	    rdata1 <= 0;
-	    rdata2 <= 0;
     end
     else begin
 	    for(i = 0; i < 32; i = i + 1) begin
-	    	reg[i] <= reg_n[i];
+	    	gpr[i] <= gpr_n[i];
 	    end
-	    rdata1 <= reg[raddr1];
-	    rdata2 <= reg[raddr2];
     end
 end
 
-always@* begin
+always @(*) begin
 	for(i = 0; i < 32; i = i + 1) begin
-		reg_n[i] = (write && (i == waddr)) ? wdata : reg[i];
+		gpr_n[i] = (wen && (i == waddr)) ? wdata : gpr[i];
 	end
 end
 
